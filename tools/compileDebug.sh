69777c24e867235cd9fb9f71c5c13e7ac32e04f3
cgeoffroy <cgeoffroy@users.noreply.github.com>
2015-01-26 15:00:53 +0000

    Squashed 'src/external/jstack/' content from commit 6afec3c
    git-subtree-dir: src/external/jstack
    git-subtree-split: 6afec3c29ced3a68e42bdf5970fb54b8aed2b115

---
+++ tools/compileDebug.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+OUTPUT=../release/jstack.js
+rm $OUTPUT
+cat ../src/JSTACK.js >> $OUTPUT
+cat ../src/JSTACK.Comm.js >> $OUTPUT
+cat ../src/JSTACK.Utils.js >> $OUTPUT
+cat ../src/JSTACK.Keystone.js >> $OUTPUT
+cat ../src/JSTACK.Nova.js >> $OUTPUT
+cat ../src/JSTACK.Cinder.js >> $OUTPUT
+cat ../src/JSTACK.Glance.js >> $OUTPUT
+cat ../src/JSTACK.Neutron.js >> $OUTPUT
