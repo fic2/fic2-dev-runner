dbbc199844a7c7f3b168f3376e5bbfa0e68be7b8
cgeoffroy <cgeoffroy@users.noreply.github.com>
2015-02-03 12:54:23 +0000

    Updating script to launch the dev container

--- dev.sh
+++ dev.sh
@@ -1,3 +1,4 @@
 #! /bin/bash -e
 
-docker run -it -p '9000:9000' -p '35729:35729' -u "$(id -u):$(id -u)" -e 'HOME=/tmp' --rm=true -v "$(pwd):/tmp/dev" -w '/tmp/dev' 'fic2-dhub-dev'
+#-p '9000:9000' -p '35729:35729'
+docker run -it -u "$(id -u):$(id -u)" -e 'HOME=/tmp' --rm=true -v "$(pwd):/tmp/dev" -w '/tmp/dev' --name 'dhub' 'fic2-dhub-dev'
