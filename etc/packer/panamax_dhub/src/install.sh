1c8058c18d9cf76ff5bd5c8be1f05d1ca7a57bfc
cgeoffroy <cgeoffroy@users.noreply.github.com>
2015-03-04 02:52:05 +0100

    Updating packer build

--- etc/packer/panamax_dhub/src/install.sh
+++ etc/packer/panamax_dhub/src/install.sh
@@ -3,9 +3,16 @@
 set -x
 
 docker pull google/cadvisor:latest
-docker pull centurylink/panamax-api:latest
-docker pull centurylink/panamax-ui:latest
+# docker pull centurylink/panamax-api:latest
+# docker pull centurylink/panamax-ui:latest
+docker pull fic2/panamax-api
+docker pull fic2/panamax-ui
+
 
 docker pull cgeoffroy/nginx-auto_cert
+
+docker pull fic2/ppnet
+docker pull mysql
+docker pull wordpress
 
 sync
