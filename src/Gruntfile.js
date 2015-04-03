4f8adf1e85ddd645875f78964574f27acd4cbc90
cgeoffroy <cgeoffroy@users.noreply.github.com>
2015-04-03 09:50:14 +0000

    Adding angular directives to the copy task

--- src/Gruntfile.js
+++ src/Gruntfile.js
@@ -331,7 +331,8 @@ module.exports = function (grunt) {
               'views/{,*/}*.html',
               'images/{,*/}*.{webp}',
               'fonts/{,*/}*.*',
-              '*.json'
+              '*.json',
+              'scripts/directives/{,*/}*.html'
             ] },
           { expand: true,
             cwd: '.tmp/images',
